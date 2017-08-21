f390363f3df118f7420c7dce9e0411a386af15ae
Reem Qais <reemq86@hotmail.com>
2017-08-21 15:08:42 +0300

    Improve error handling #23

--- src/controllers/places.js
+++ src/controllers/places.js
@@ -5,7 +5,7 @@ const placeController = {};
 placeController.getAll = (req, res) => {
   const placesURL = 'https://nazareth-open-tourism-platform.herokuapp.com/places';
   Request(placesURL, (error, response, body) => {
-    if (error) return console.log(error);
+    if (error) return res.status(404).send(error);
     const data = JSON.parse(body);
     res.render('places', {output: data});
   });
