e40f33ce9001b4e483b2037699c19cebf05eca5e
Reem Qais <reemq86@hotmail.com>
2017-08-24 22:14:44 +0300

    Check if query param is only en or ar #23

--- src/controllers/places.js
+++ src/controllers/places.js
@@ -5,6 +5,10 @@ const { placesURL } = require('../constants/urls.json');
 const placeController = {};
 
 placeController.getAll = (req, res) => {
+  if (req.params.lang !== 'en' || req.params.lang !== 'ar') {
+    return res.status(404).send('Page does not exist');
+  }
+
   Request(placesURL, (error, response, body) => {
     if (error) return res.status(404).send(error);
 
