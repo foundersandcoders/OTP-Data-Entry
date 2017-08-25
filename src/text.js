950db2b21798aa02a267212a77b5f142c6207401
Reem Qais <reemq86@hotmail.com>
2017-08-25 12:32:40 +0300

    Localise page title in main.hbs #23

--- src/text.js
+++ src/text.js
@@ -1,11 +1,13 @@
 module.exports = {
   en: {
+    pageTitle: 'OTP Data Entry',
     placesTitle: 'Places',
     placesView: 'View',
     placesEdit: 'Edit',
     placesImage: 'Place Image'
   },
   ar: {
+    pageTitle: 'إدخال البيانات',
     placesTitle: 'أماكن',
     placesView: 'إعرض',
     placesEdit: 'حرر',
