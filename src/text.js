3710db30e48af71e94a48a157b7fe8a84c34b046
Reem Qais <reemq86@hotmail.com>
2017-09-05 16:06:21 +0300

    Fix indentation #13

--- src/text.js
+++ src/text.js
@@ -4,13 +4,35 @@ module.exports = {
     placesTitle: 'Places',
     placesView: 'View',
     placesEdit: 'Edit',
-    placesImage: 'Place Image'
-  },
+    placesImage: 'Place Image',
+    placeName: 'Name',
+    placeDescription: 'Description',
+    placeAddress: 'Address',
+    placeHours: 'Opening Hours',
+    placeLocation: 'Location',
+    placeImage: 'Image',
+    placeWebsite: 'Website',
+    placePhone: 'Phone',
+    placeEmail: 'Email',
+    placeAccessibility: 'Accessibility Options',
+    placeCategories: 'Categories'
+    },
   ar: {
     pageTitle: 'إدخال البيانات',
     placesTitle: 'أماكن',
     placesView: 'إعرض',
     placesEdit: 'حرر',
-    placesImage: 'صورة المكان'
+    placesImage: 'صورة المكان',
+    placeName: 'الإسم',
+    placeDescription: 'وصف',
+    placeAddress: 'العنوان',
+    placeHours: 'ساعات العمل',
+    placeLocation: 'الموقع على الخارطة',
+    placeImage: 'صورة',
+    placeWebsite: 'الموقع',
+    placePhone: 'الهاتف',
+    placeEmail: 'البريد الإلكترونيّ',
+    placeAccessibility: 'إمكانية الوصول',
+    placeCategories: 'فئات'
   }
 };
