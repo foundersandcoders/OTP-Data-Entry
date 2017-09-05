7c4cb89320b86926d263a27f50320e8ac347943b
Reem Qais <reemq86@hotmail.com>
2017-09-05 13:58:21 +0300

    Add switch statement to get specific place function #13

--- src/controllers/places.js
+++ src/controllers/places.js
@@ -51,10 +51,22 @@ placeController.getSpecific = (req, res) => {
     const place = JSON.parse(body);
     place.local = place[req.params.lang];
 
+    let dir = '';
+    switch (req.params.lang) {
+      case 'en':
+        dir = 'ltr';
+        break;
+      case 'ar':
+        dir = 'rtl';
+        break;
+      default: dir = 'rtl';
+    }
+
     res.render('place', {
       place,
       localLang: req.app.locals[req.params.lang],
-      lang: req.params.lang
+      lang: req.params.lang,
+      dir
     });
   });
 };
