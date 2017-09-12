5b849652a5050a59c2d9f382c8e539cf2316d420
suha-edris <suha.edris@gmail.com>
2017-09-12 18:22:32 +0300

    Add post route

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -6,5 +6,6 @@ router.get('/', require('./home.js'));
 router.get('/:lang/places', placeController.getAll);
 router.get('/:lang/place/:id', placeController.getSpecific);
 router.get('/:lang/add-place', placeController.renderForm);
+router.post('/:lang/add-place', placeController.addPlace);
 
 module.exports = router;
