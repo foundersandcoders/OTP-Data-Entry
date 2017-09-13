4c5efdebc9c23d49072418add60b0f31c2f43880
suha-edris <suha.edris@gmail.com>
2017-09-13 16:50:36 +0300

    move helpers folder to src

---
+++ src/helpers/getLatLng.js
@@ -0,0 +1,22 @@
+const queryString = require('querystring');
+const request = require('request');
+
+const getLatLng = (address, cb) => {
+  const baseURI = 'https://maps.googleapis.com/maps/api/geocode/json?';
+  const queryParams = queryString.stringify({
+    address: address,
+    key: process.env.GOOGLE_API
+  });
+  const requestURI = baseURI + queryParams;
+  request(requestURI, (err, res, body) => {
+    if (err) cb(err);
+    const location = JSON.parse(body).results[0];
+    if (location) {
+      cb(null, JSON.parse(body).results[0].geometry.location);
+    } else {
+      cb(null, { lat: 32.7014255, lng: 35.2967795 });
+    }
+  });
+};
+
+module.exports = getLatLng;
