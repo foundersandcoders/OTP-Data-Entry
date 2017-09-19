d4bd645572ed89fd76b267a0b557776f5a0d5c64
suha-edris <suha.edris@gmail.com>
2017-09-19 09:48:45 +0300

    add edit-place router

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -7,5 +7,6 @@ router.get('/:lang/places', placeController.getAll);
 router.get('/:lang/place/:id', placeController.getSpecific);
 router.get('/:lang/add-place', placeController.renderForm);
 router.post('/:lang/add-place', placeController.addPlace);
+router.get('/:lang/edit-place/:id', placeController.getSpecific);
 
 module.exports = router;
