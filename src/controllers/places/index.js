7fce4f57955e67725fee951efc5fa61ebfe43a8a
suha-edris <suha.edris@gmail.com>
2017-09-24 18:20:58 +0300

    change the name of themodule

--- src/controllers/places/index.js
+++ src/controllers/places/index.js
@@ -2,7 +2,7 @@ const placeController = {
   getAll: require('./get_all.js'),
   getSpecific: require('./get_specific.js'),
   renderForm: require('./render_form.js'),
-  addPlace: require('./add_place.js'),
+  addPlace: require('./modify_content.js'),
   renderEditForm: require('./render_edit_form.js')
 };
 
