21646787a129dd7aa2056f2033ab4f35249c8a01
suha-edris <suha.edris@gmail.com>
2017-09-24 15:55:54 +0300

    change/add router for edit palce

--- src/controllers/places/index.js
+++ src/controllers/places/index.js
@@ -2,7 +2,8 @@ const placeController = {
   getAll: require('./get_all.js'),
   getSpecific: require('./get_specific.js'),
   renderForm: require('./render_form.js'),
-  addPlace: require('./add_place.js')
+  addPlace: require('./add_place.js'),
+  renderEditForm: require('./render_edit_form.js')
 };
 
 module.exports = placeController;
