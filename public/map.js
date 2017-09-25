a2211c60ee73c996941ae48b6138f69b63ae9e4d
suha-edris <suha.edris@gmail.com>
2017-09-25 13:28:53 +0300

    fix the map coordinates

--- public/map.js
+++ public/map.js
@@ -3,13 +3,13 @@ function placeMap () {
   var lat = coords[0];
   var lng = coords[1];
   var mapOptions = {
-    center: new google.maps.LatLng(lng, lat),
+    center: new google.maps.LatLng(lat, lng),
     zoom: 18,
     disableDefaultUI: true
   };
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(lng, lat),
+    position: new google.maps.LatLng(lat, lng),
     map: map
   });
 }
