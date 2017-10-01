a00deb192f38bb85e6b04b8c88be3cee5796058f
suha-edris <suha.edris@gmail.com>
2017-10-01 20:10:41 +0300

    functionality to delete an event

---
+++ src/controllers/events/delete_event.js
@@ -0,0 +1,26 @@
+const Request = require('request');
+const {eventsURL} = require('../../constants/urls.json');
+
+module.exports = (req, res) => {
+  const reqOptions = {
+    url: `${eventsURL}/${req.params.id}`,
+    method: 'DELETE'
+  };
+
+  Request(reqOptions, (error, response) => {
+    if (error) {
+      return res.render('error', {
+        statusCode: 500,
+        errorMessage: res.locals.localText.serverError
+      });
+    }
+    if (response.statusCode !== 204) {
+      return res.render('error', {
+        statusCode: 400,
+        errorMessage: res.locals.localText.badRequest
+      });
+    } else {
+      res.redirect(`/${req.params.lang}/events`);
+    }
+  });
+};
