eeb85501bda2a58d9869b7336b90dd36dc12e422
suha-edris <suha.edris@gmail.com>
2017-10-05 19:03:17 +0300

    fix module name

--- src/controllers/events/index.js
+++ src/controllers/events/index.js
@@ -4,7 +4,7 @@ const eventsController = {
   deleteEvent: require('./delete_event.js'),
   addEvent: require('./modify_content.js'),
   renderForm: require('./render_form.js'),
-  renderEditForm: require('./modify_content.js'),
+  renderEditForm: require('./render_edit_form.js'),
   placesList: require('../../middleware/getPlaceNameAndId.js')
 };
 
