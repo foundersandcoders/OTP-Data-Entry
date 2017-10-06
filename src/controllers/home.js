8ff4a3f0e63672313269af7c7cea40281a6544ac
suha-edris <suha.edris@gmail.com>
2017-10-06 11:47:28 +0300

    render english and arabic text

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,8 @@
+const text = require('../text.js');
+
 module.exports = (req, res) => {
-  res.render('home');
+  res.render('home', {
+    english: text.en.english,
+    arabic: text.ar.arabic
+  });
 };
