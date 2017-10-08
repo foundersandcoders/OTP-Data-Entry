9105c2ff63b22cead344fe30035065fa6982705e
suha-edris <suha.edris@gmail.com>
2017-10-08 13:09:00 +0300

    make smallll changes

--- src/content.js
+++ src/content.js
@@ -1,5 +1,3 @@
-// const text = require('./text.js');
-
 module.exports = (req, res) => {
   res.render('content');
 };
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,3 @@
 module.exports = (req, res) => {
-  res.render('home', {
-    english: req.app.locals.en.english,
-    arabic: req.app.locals.ar.arabic
-  });
+  res.render('home');
 };
