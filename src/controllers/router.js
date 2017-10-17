26f3727cd74aa7d8375b11496eeb17333a8c2b6f
suha-edris <suha.edris@gmail.com>
2017-10-17 20:13:27 +0300

    add middleware to add and edit routers

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -4,6 +4,7 @@ const eventsController = require('./events/index.js');
 const placesList = require('../middleware/getPlaceNameAndId.js');
 const oauthCode = require('./OAuth/code.js');
 const oauthToken = require('./OAuth/token.js');
+const checkLoggedIn = require('../middleware/checkLoggedIn.js');
 
 router.get('/', require('./home.js'));
 router.get('/:lang/content', require('../content.js'));
@@ -13,18 +14,18 @@ router.get('/:lang/places', placeController.getAll);
 router.get('/:lang/place/:id', placeController.getSpecific);
 router.get('/:lang/add-place', placeController.renderForm);
 router.post('/:lang/add-place', placeController.addPlace);
-router.get('/:lang/edit-place/:id', placeController.renderEditForm);
-router.post('/:lang/edit-place/:id', placeController.addPlace);
-router.get('/:lang/delete-place/:id', placeController.deletePlace);
+router.get('/:lang/edit-place/:id', checkLoggedIn, placeController.renderEditForm);
+router.post('/:lang/edit-place/:id', checkLoggedIn, placeController.addPlace);
+router.get('/:lang/delete-place/:id', checkLoggedIn, placeController.deletePlace);
 
 // events
 router.get('/:lang/events', eventsController.getAll);
 router.get('/:lang/event/:id', eventsController.getSpecific);
-router.get('/:lang/delete-event/:id', eventsController.deleteEvent);
+router.get('/:lang/delete-event/:id', checkLoggedIn, eventsController.deleteEvent);
 router.get('/:lang/add-event', placesList, eventsController.renderForm);
 router.post('/:lang/add-event', eventsController.addEvent);
-router.get('/:lang/edit-event/:id', placesList, eventsController.renderEditForm);
-router.post('/:lang/edit-event/:id', eventsController.addEvent);
+router.get('/:lang/edit-event/:id', checkLoggedIn, placesList, eventsController.renderEditForm);
+router.post('/:lang/edit-event/:id', checkLoggedIn, eventsController.addEvent);
 
 router.get('/:lang/loginOauth', oauthCode);
 router.get('/oauth/token', oauthToken);
