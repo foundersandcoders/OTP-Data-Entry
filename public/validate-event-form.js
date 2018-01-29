3be10091e412a443dad48d08f87c8700696ce4b2
mario <mario.saliba98@gmail.com>
2018-01-29 15:57:41 +0200

    prevent the user from submitting the form if a photo is uploading rel #86

--- public/validate-event-form.js
+++ public/validate-event-form.js
@@ -24,6 +24,13 @@
       cost: elements.cost.value,
     };
 
+    // checks if a file is being uploaded
+    if (
+      document.getElementById('spinner').classList.value.indexOf('dn') === -1
+    ) {
+      return notValid('*Image did not finish upload');
+    }
+
     data.imageUrl = elements.s3Url
       ? elements.s3Url.value
       : elements.imageUrl.value;
