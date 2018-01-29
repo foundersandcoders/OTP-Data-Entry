857ac6f83fc98e31413d1b5a0f70bb9eef3f216f
mario <mario.saliba98@gmail.com>
2018-01-29 16:51:23 +0200

    use composition for calling functions rel #86

--- public/event-image-upload.js
+++ public/event-image-upload.js
@@ -9,34 +9,32 @@
     var fileInputFiles = fileInput.files;
     var file = fileInputFiles[0];
     imagePreview.src && (imagePreview.src = '');
-    getSignedRequest(file);
     spinner.classList.toggle('dn');
     imagePreview.classList.add('dn');
-  };
-
-  function getSignedRequest(file) {
-    file &&
-      axios
-        .get('/sign-s3?file-name=' + file.name + '&file-type=' + file.type)
-        .then(function(res) {
-          uploadFile(file, res.data.signedRequest, res.data.url);
-        })
-        .catch(function(err) {
-          fileErrorMessage.textContent = 'Could not upload file';
-        });
-  }
-
-  function uploadFile(file, signedRequest, url) {
-    axios
-      .put(signedRequest, file)
+    getSignedRequest(file)
       .then(function(res) {
+        imagePreview.src = res.data.url;
+        hiddenFileInput.value = res.data.url;
+        return uploadFile(res.data.signedRequest, file);
+      })
+      .then(function() {
         spinner.classList.toggle('dn');
         imagePreview.classList.remove('dn');
-        imagePreview.src = url;
-        hiddenFileInput.value = url;
       })
       .catch(function(err) {
+        imagePreview.src = '';
+        hiddenFileInput.value = '';
         fileErrorMessage.textContent = 'Could not upload file';
       });
-  }
+
+    function getSignedRequest(file) {
+      return axios.get(
+        '/sign-s3?file-name=' + file.name + '&file-type=' + file.type,
+      );
+    }
+
+    function uploadFile(signedRequest, file) {
+      return axios.put(signedRequest, file);
+    }
+  };
 })();
