011d0f3d8bb0f56c37ef407ff42c6bd7fb942b91
mario <mario.saliba98@gmail.com>
2018-03-13 14:42:28 +0200

    implement the same changes from the places controller rel #111

--- src/otp_sdk/events.js
+++ src/otp_sdk/events.js
@@ -1,11 +1,28 @@
 const axios = require('axios');
+const { eventsURL } = require('../constants/urls.json');
 
-module.exports.modify = (options, tools) => {
+module.exports.update = (options, id) => {
+  console.log(options);
   return new Promise(async (resolve, reject) => {
+    let reqOpts;
+    switch (options.method) {
+      case 'post':
+        reqOpts = Object.assign({}, options, { url: eventsURL });
+        break;
+      case 'put':
+        reqOpts = Object.assign({}, options, {
+          url: `${eventsURL}/${id}`,
+        });
+        break;
+      default:
+        reject();
+    }
+
     try {
-      await axios(options);
-      resolve();
+      const newEvent = await axios(reqOpts);
+      resolve(newEvent.data);
     } catch (error) {
+      console.log(error);
       error.response.data.error === 'Unauthorized'
         ? reject({ Unauthorized: true })
         : reject();
